5b3cb69b6cef4e8232c72debea5fde453fbd35ff
edatencio <ed.atencio16@gmail.com>
2019-01-26 18:27:40 -0400

    Made cinemachine camera shake

--- Assets/Scripts/CinemachineCameraShake.cs
+++ Assets/Scripts/CinemachineCameraShake.cs
@@ -1,17 +1,31 @@
 using UnityEngine;
+using Cinemachine;
+using NaughtyAttributes;
 
-public class PlayerPosition : MonoBehaviour
+public class CinemachineCameraShake : MonoBehaviour
 {
      /*************************************************************************************************
      *** Variables
      *************************************************************************************************/
-     [SerializeField] private Vector3Asset playerPosition;
+     [SerializeField] private CinemachineVirtualCamera virtualCamera;
+     [SerializeField] private float duration;
+     [SerializeField] private float aplitude;
+     [SerializeField] private float frequency;
+     [SerializeField] private bool shakeOnAwake;
+
+     private CinemachineBasicMultiChannelPerlin noise;
+     private Timer timer;
 
      /*************************************************************************************************
      *** Start
      *************************************************************************************************/
      private void Start()
      {
+          timer = new Timer();
+          noise = virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+          if (shakeOnAwake)
+               Shake();
      }
 
      /*************************************************************************************************
@@ -19,21 +33,37 @@ public class PlayerPosition : MonoBehaviour
      *************************************************************************************************/
      private void Update()
      {
-          playerPosition.Value = transform.position;
-     }
-
-     /*************************************************************************************************
-     *** FixedUpdate
-     *************************************************************************************************/
-     private void FixedUpdate()
-     {
+          if (timer.isRunning && timer.ElapsedSeconds > duration)
+          {
+               timer.Stop();
+               Stop();
+          }
      }
 
      /*************************************************************************************************
      *** Properties
      *************************************************************************************************/
+     public bool isRunning
+     {
+          get { return timer.isRunning; }
+     }
 
      /*************************************************************************************************
      *** Methods
      *************************************************************************************************/
+     [Button]
+     public void Shake()
+     {
+          noise.m_AmplitudeGain = aplitude;
+          noise.m_FrequencyGain = frequency;
+          timer.Start();
+     }
+
+     [Button]
+     public void Stop()
+     {
+          noise.m_AmplitudeGain = 0f;
+          noise.m_FrequencyGain = 0f;
+          timer.Stop();
+     }
 }
