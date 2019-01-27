9c90ac5aba9eeede06bd380602debf1c5ee36207
edatencio <ed.atencio16@gmail.com>
2019-01-27 01:16:37 -0400

    Done building transparency

---
+++ Assets/Scripts/BuildingFade.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using NaughtyAttributes;
+
+public class BuildingFade : MonoBehaviour
+{
+     /*************************************************************************************************
+     *** Variables
+     *************************************************************************************************/
+     [SerializeField] private Renderer rend;
+     [SerializeField] private Camera mainCamera;
+     [SerializeField] private Vector3Asset playerPosition;
+
+     private enum State { Opaque, Transparent }
+     private State state;
+
+     /*************************************************************************************************
+     *** Start
+     *************************************************************************************************/
+     private void Start()
+     {
+          state = State.Transparent;
+          Opaque();
+     }
+
+     /*************************************************************************************************
+     *** FixedUpdate
+     *************************************************************************************************/
+     private void FixedUpdate()
+     {
+          RaycastHit hit;
+          Vector3 direction = (playerPosition.Value - mainCamera.transform.position).normalized;
+          Physics.Raycast(mainCamera.transform.position, direction, out hit);
+
+          if (hit.collider.tag == Constants.Player)
+               Opaque();
+          else
+               Transparent();
+     }
+
+     /*************************************************************************************************
+     *** Properties
+     *************************************************************************************************/
+
+     /*************************************************************************************************
+     *** Methods
+     *************************************************************************************************/
+     [Button]
+     private void Transparent()
+     {
+          if (state != State.Transparent)
+          {
+               state = State.Transparent;
+               Color color = rend.material.color;
+               color.a = 0.2f;
+               rend.material.SetColor("_Color", color);
+               rend.material.ChangeRenderMode(StandardShaderUtils.BlendMode.Transparent);
+          }
+     }
+
+     [Button]
+     private void Opaque()
+     {
+          if (state != State.Opaque)
+          {
+               state = State.Opaque;
+
+               Color color = rend.material.color;
+               color.a = 1f;
+               rend.material.SetColor("_Color", color);
+               rend.material.ChangeRenderMode(StandardShaderUtils.BlendMode.Opaque);
+          }
+     }
+}
